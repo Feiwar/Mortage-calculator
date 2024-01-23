11183355889a2d8701915f0bb92237aec8f28ce5
Feiwar <143819364+Feiwar@users.noreply.github.com>
2024-01-23 16:06:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def calculate_mortgage():
+    # здесь код для расчета ипотечных платежей
+    pass
+
+def open_mortgage_calculator():
+    window = tk.Tk()
+    window.title("Ипотечный калькулятор")
+
+    # добавляем виджеты (например: текстовые поля, метки, кнопки) для ввода данных
+
+    window.mainloop()
+
+open_mortgage_calculator()
